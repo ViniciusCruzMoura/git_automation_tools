a276d91d041fa54cfb5be2844432a65a90b4e84b
Vinícius Moura <83794355+ViniciusCruzMoura@users.noreply.github.com>
2024-02-16 00:39:44 -0400

    Create push.sh

---
+++ push.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# SOURCE_BRANCH="main"
+# NEW_BRANCH="update-$USER-$(date +'%Y%m%d%H%M%S')"
+SOURCE_BRANCH=$(git branch --show-current)
+NEW_BRANCH="update-$USER-$(date +'%Y%m%d')"
+
+git fetch --all
+git pull origin $SOURCE_BRANCH
+git add .
+
+# git restore --staged push.sh
+git status
+
+while [[ -z "$COMMIT_MSG" ]]; do
+    echo 'Enter the commit message:'
+    read COMMIT_MSG
+done
+
+git commit -m "$COMMIT_MSG"
+git push origin $SOURCE_BRANCH:$NEW_BRANCH
+
+if [ $SOURCE_BRANCH != $NEW_BRANCH ]; then
+    git checkout -b $NEW_BRANCH
+    git reset --hard origin/$NEW_BRANCH
+fi
+
+git fetch --prune --prune-tags
