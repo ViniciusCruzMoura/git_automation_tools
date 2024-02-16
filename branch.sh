266f6d4b018f294dbcac2b374f17b8d2de5d2f6c
Vinícius Moura <83794355+ViniciusCruzMoura@users.noreply.github.com>
2024-02-16 15:16:02 -0400

    Create branch.sh

---
+++ branch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PROD_BRANCH=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+NEW_BRANCH="update-$USER-$(date +'%y%m%d')"
+
+git fetch --all --prune --prune-tags
+
+git checkout $PROD_BRANCH
+git reset --hard origin/$PROD_BRANCH
+
+git checkout -B $NEW_BRANCH
