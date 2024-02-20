4f004cbd9e6dcc993d8d2c6ce9642a8eaeff77bd
Vinícius Moura <83794355+ViniciusCruzMoura@users.noreply.github.com>
2024-02-20 13:57:13 -0400

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,101 +1,106 @@
 branch() {
-    PROD_BRANCH=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
-    NEW_BRANCH="update-$USER-$(date +'%y%m%d')"
-
-    git fetch --all --prune --prune-tags
-
-    git checkout $PROD_BRANCH
-    git reset --hard origin/$PROD_BRANCH
-
-    git checkout -B $NEW_BRANCH
-
-    git push origin $NEW_BRANCH
-
-    echo "Created and checked out new branch '$NEW_BRANCH' based on main"
+	PROD_BRANCH=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+	NEW_BRANCH="update-$USER-$(date +'%y%m%d')"
+	
+	git fetch --all --prune --prune-tags
+	
+	git checkout $PROD_BRANCH
+	git reset --hard origin/$PROD_BRANCH
+	
+	git checkout -B $NEW_BRANCH
+	
+	git push origin $NEW_BRANCH
+	
+	echo "Created and checked out new branch '$NEW_BRANCH' based on main"
 }
 
 merge() {
-    SOURCE_BRANCH=$(git branch --show-current)
-    PROD_BRANCH=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
-    MERGE_BRANCH="merge-$USER-$(date +'%y%m%d')"
-
-    git fetch --all --prune --prune-tags
-
-    # Ensure we're on the main branch
-    # git checkout $PROD_BRANCH
-
-    # Create a new branch based on the main branch
-    git checkout -B $MERGE_BRANCH
-
-    # Pull the latest changes from the main branch
-    git pull origin $PROD_BRANCH
-    # git reset --hard origin/$PROD_BRANCH
-
-    # Merge the specified branch into main
-    git merge $SOURCE_BRANCH
-
-    # Push the merged changes to the remote repository
-    git push origin $MERGE_BRANCH
-
-    # Optionally, you can delete the merged branch
-    # git branch -D $SOURCE_BRANCH
-
-    git checkout $SOURCE_BRANCH
-
-    git fetch --all --prune --prune-tags
-
-    echo "Merge complete"
+	SOURCE_BRANCH=$(git branch --show-current)
+	PROD_BRANCH=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+	MERGE_BRANCH="merge-$USER-$(date +'%y%m%d')"
+	
+	git fetch --all --prune --prune-tags
+	
+	# Ensure we're on the main branch
+	# git checkout $PROD_BRANCH
+	
+	# Create a new branch based on the main branch
+	git checkout -B $MERGE_BRANCH
+	
+	# Pull the latest changes from the main branch
+	git pull origin $PROD_BRANCH
+	# git reset --hard origin/$PROD_BRANCH
+	
+	# Merge the specified branch into main
+	git merge $SOURCE_BRANCH
+	
+	# Push the merged changes to the remote repository
+	git push origin $MERGE_BRANCH
+	
+	# Optionally, you can delete the merged branch
+	# git branch -D $SOURCE_BRANCH
+	
+	git checkout $SOURCE_BRANCH
+	
+	git fetch --all --prune --prune-tags
+	
+	echo "Merge complete"
 }
 
 push() {
-    # SOURCE_BRANCH="main"
-    # NEW_BRANCH="update-$USER-$(date +'%Y%m%d%H%M%S')"
-    SOURCE_BRANCH=$(git branch --show-current)
-    # NEW_BRANCH="update-$USER-$(date +'%y%m%d')"
-
-    git fetch --all
-    git pull origin $SOURCE_BRANCH
-    git add .
-
-    # git restore --staged push.sh
-    git status
-
-    while [[ -z "$COMMIT_MSG" ]]; do
-        echo 'Enter the commit message:'
-        read COMMIT_MSG
-    done
-
-    git commit -m "$COMMIT_MSG"
-    # git push origin $SOURCE_BRANCH:$NEW_BRANCH
-
-    # if [ $SOURCE_BRANCH != $NEW_BRANCH ]; then
-    #     git checkout -b $NEW_BRANCH
-    #     git reset --hard origin/$NEW_BRANCH
-    # fi
-    git push origin $SOURCE_BRANCH
-
-    git fetch --prune --prune-tags
-
-    echo "Push complete"
+	# SOURCE_BRANCH="main"
+	# NEW_BRANCH="update-$USER-$(date +'%Y%m%d%H%M%S')"
+	SOURCE_BRANCH=$(git branch --show-current)
+	# NEW_BRANCH="update-$USER-$(date +'%y%m%d')"
+	
+	git fetch --all
+	git pull origin $SOURCE_BRANCH
+	git add .
+	
+	# git restore --staged push.sh
+	git status
+	
+	while [[ -z "$COMMIT_MSG" ]]; do
+		echo 'Enter the commit message:'
+		read COMMIT_MSG
+	done
+	
+	git commit -m "$COMMIT_MSG"
+	# git push origin $SOURCE_BRANCH:$NEW_BRANCH
+	
+	# if [ $SOURCE_BRANCH != $NEW_BRANCH ]; then
+	#     git checkout -b $NEW_BRANCH
+	#     git reset --hard origin/$NEW_BRANCH
+	# fi
+	git push origin $SOURCE_BRANCH
+	
+	git fetch --prune --prune-tags
+	
+	echo "Push complete"
 }
 
 tag() {
-    # git tag -l | xargs git tag -d
-    # git fetch --tags
-    git fetch --prune --prune-tags
-
-    while [[ -z "$TAG" ]]; do
-        echo "Enter the tag version:"
-        read TAG;
-    done
-
-    git tag $TAG
-    git push origin $TAG
+	# git tag -l | xargs git tag -d
+	# git fetch --tags
+	git fetch --prune --prune-tags
+	
+	while [[ -z "$TAG" ]]; do
+		echo "Enter the tag version:"
+		read TAG;
+	done
+	
+	git tag $TAG
+	git push origin $TAG
 }
 
 case $1 in
 	-help|--help|help)
 		echo "These are common commands used in various situations:"
+		echo "help"
+		echo "branch"
+		echo "merge"
+		echo "push"
+		echo "tag"
 		exit 0
 		;;
 	-branch|--branch|branch)
@@ -106,11 +111,11 @@ case $1 in
 		merge
 		exit 0
 		;;
-    -push|--push|push)
+	-push|--push|push)
 		push
 		exit 0
 		;;
-    -tag|--tag|tag)
+	-tag|--tag|tag)
 		tag
 		exit 0
 		;;
